0cdef6c60bb3b2cff90535b53b7169dfd140e134
eduse <edu.serban26@gmail.com>
2023-02-03 16:50:36 +0200

    pls

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,26 +1,34 @@
-import { getUserWithUsername } from 'import/lib/firebase';
+import { collection, getDocs, limit, orderBy, where } from 'firebase/firestore';
+import { firestore, getUserWithUsername, textToJSON } from 'import/lib/firebase';
 import React from 'react'
 
 export async function getServerSideProps({ query }){
   const { username } = query;
 
+  let user = null;
+  let posts = null;
+
   const userDoc = await getUserWithUsername(username);
-  let user = userDoc.data();
+  if(userDoc){
+    user = userDoc.data();
+    const postsRef = collection(firestore, 'posts');
+    const q = query(postsRef, where('published', '===', true), 
+      orderBy('createdAt', 'desc'),
+      limit(5));
+    posts = (await getDocs(q).map(textToJSON))
+  }
   console.log(user.displayName);
 
   return {
-    props: { username, user },
+    props: { user, posts },
   };
 }
 
 export default function index({username, user}) {
 
   return (
-    <>
-      <div>{username}</div>
-      <div>{user.displayName}</div>
-      <div>{user.photoURL}</div>
-      <div>omgomgomgomg</div>
-    </>
+    <main>
+      <UserProfile user={ user } />
+    </main>
   );
 }
