de579484421252a310f9de950026fe3e62d864cc
eduse <edu.serban26@gmail.com>
2023-04-25 21:04:48 +0300

    omgWorks

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -5,21 +5,18 @@ export default function PostFeed({ posts, admin }) {
 }
 
 function PostItem({ post, admin = false }) {
-  // Naive method to calc word count and read time
   const wordCount = post?.content.trim().split(/\s+/g).length;
   const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
   return (
     <div className="card">
       <Link href={`/${post.username}`}>
-        <a>
           <strong>By @{post.username}</strong>
-        </a>
       </Link>
 
       <Link href={`/${post.username}/${post.slug}`}>
         <h2>
-          <a>{post.title}</a>
+          {post.title}
         </h2>
       </Link>
 
@@ -30,7 +27,6 @@ function PostItem({ post, admin = false }) {
         <span className="push-left">💗 {post.heartCount || 0} Hearts</span>
       </footer>
 
-      {/* If admin view, show extra controls for user */}
       {admin && (
         <>
           <Link href={`/admin/${post.slug}`}>
